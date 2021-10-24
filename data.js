2968935c8d5ce005eba62f6973a8b28053c4488c
kratoon3 <ondrej@kratochvil.dev>
2021-10-24 11:47:46 +0200

    Update to https.

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
 const locations = [
     {id: 'neklid', name: 'Neklid', url: 'https://www.neklid.com/#daily-menu'},
-    {id: 'redchilli', name: 'Red Chilli', url: 'http://redchilli.cz/poledni-menu/'},
-    {id: 'latorretta', name: 'La Torretta', url: 'http://latorretta.cz/moskevska/?page_id=3674'},
+    {id: 'redchilli', name: 'Red Chilli', url: 'https://redchilli.cz/poledni-menu/'},
+    {id: 'latorretta', name: 'La Torretta', url: 'https://latorretta.cz/moskevska/?page_id=3674'},
     {id: 'gulliverbar', name: 'Gulliver Bar', url: 'https://gulliverbar.cz/poledni-menu/'},
-    {id: 'ukuratka', name: 'U Kuřátka', url: 'http://www.ukuratka.cz/'},
+    {id: 'ukuratka', name: 'U Kuřátka', url: 'https://www.ukuratka.cz/'},
     {id: 'phohadola', name: 'Hadola', url: 'https://www.phohadola.cz/'},
-    {id: 'uklokone', name: 'U Klokoně', url: 'http://www.restauraceuklokone.cz/'},
+    {id: 'uklokone', name: 'U Klokoně', url: 'https://www.restauraceuklokone.cz/'},
     {id: 'tramtarie', name: 'Tramtárie', url: 'https://www.tramtarierestaurace.cz/j%C3%ADdeln%C3%AD-l%C3%ADstek'}
 ];
